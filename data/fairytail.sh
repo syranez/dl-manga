1e5b7ca91446ade5ac028e5a4860679193124eff
Michael Nowack <syranez@minaga-church.de>
2012-05-18 09:21:24 +0200

    Added definition file for Fairy Tail (mangareader);

---
+++ data/fairytail.sh
@@ -0,0 +1,18 @@
+# naruto data file for mangareader
+#
+#
+
+# manga site
+SOURCE="mangareader"
+
+# name of manga on mangareader.
+NAME="fairy-tail"
+
+# number of digits of the output chapter directory name
+DIGIT_COUNT=3
+
+# local directory in which mangas will be saved.
+TARGET_DIR="${HOME}/mangareader/fairytail"
+
+# index site of manga
+INDEX_SITE="http://www.mangareader.net/135/fairy-tail.html"
