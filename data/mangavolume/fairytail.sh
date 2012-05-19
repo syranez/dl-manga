3ff21683f7912ecefa6d7b6b568bb3111261276c
Michael Nowack <syranez@minaga-church.de>
2012-05-19 23:49:24 +0200

    New Manga definition file: mangavolume / fairy tail

---
+++ data/mangavolume/fairytail.sh
@@ -0,0 +1,19 @@
+# fairy tail data file for mangavolume.com
+
+# manga site
+SOURCE="mangavolume"
+
+# name of manga on mangareader.
+NAME="fairy-tail"
+
+# number of digits of the output chapter directory name
+DIGIT_COUNT=3
+
+# local directory in which mangas will be saved.
+TARGET_DIR="${HOME}/mangareader/mangavolume-fairytail"
+
+# index site of manga
+#
+# on mangavolume the index site has to be the main page of the first chapter
+#+ cos mangavolume pages the manga chapter index.
+INDEX_SITE="www.mangavolume.com/fairy-tail/chapter-fairy-tail-1/"
