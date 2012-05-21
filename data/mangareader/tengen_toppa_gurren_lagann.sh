dfa49cf52a2378e114ba6ab76a9318030546122a
Michael Nowack <syranez@minaga-church.de>
2012-05-21 22:38:38 +0200

    Enhancement: Added manga definition file for TENGEN TOPPA GURREN LAGANN MANGA on mangareader.

---
+++ data/mangareader/tengen_toppa_gurren_lagann.sh
@@ -0,0 +1,16 @@
+# Tengen Toppa Gurren Lagann manga data file for mangareader
+
+# manga site
+SOURCE="mangareader"
+
+# name of manga on mangareader.
+NAME="tengen-toppa-gurren-lagann"
+
+# number of digits of the output chapter directory name
+DIGIT_COUNT=3
+
+# local directory in which mangas will be saved.
+TARGET_DIR="${HOME}/mangareader/tengen-toppa-gurren-lagann"
+
+# index site of manga
+INDEX_SITE="http://www.mangareader.net/821/tengen-toppa-gurren-lagann.html"
